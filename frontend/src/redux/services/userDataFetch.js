8536a24805f1e391775ed3a5a55558d4b873e9b9
RajatsGit <rajatsharma9326@gmail.com>
2023-10-13 01:16:00 +0530

    changes
    Signed-off-by: RajatsGit <rajatsharma9326@gmail.com>

--- frontend/src/redux/services/userDataFetch.js
+++ frontend/src/redux/services/userDataFetch.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const userDataFetch = createApi({
   reducerPath: 'userDataFetch',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/api/getUser/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://react-node-assignment1.onrender.com/api/getUser' }),
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => 'users',
@@ -11,6 +11,4 @@ export const userDataFetch = createApi({
   }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetUsersQuery } = userDataFetch
